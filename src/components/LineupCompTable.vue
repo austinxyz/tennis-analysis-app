f9a7a22be4c6a379a710e3c546359f70dcc0d2fe
austinxyz <Austin.xyz@gmail.com>
2022-12-01 21:34:20 -0800

    add win image

--- src/components/LineupCompTable.vue
+++ src/components/LineupCompTable.vue
@@ -14,33 +14,74 @@ export default {
 
 <template>
      <div class="flex flex-col md:flex-row md:transform md:scale-75 lg:scale-100 justify-center">
+
          <div class="border-transparent rounded-lg text-center p-5 mx-auto md:mx-0 my-2 bg-gray-100 font-medium z-10 shadow-lg">
            <div class="font-bold text-2xl"> Team {{ teamName1 }} </div>
            <hr />
-           <div class="text-sm my-3 flex flex-row"> D1 : {{lineup1.D1.pair.pairInfo}}</div>
+           <div class="text-sm my-3 flex flex-row">
+                <div>
+                    <img v-if="lineup1.D1.pair.totalUTR > lineup2.D1.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+                </div>
+           D1 : {{lineup1.D1.pair.pairInfo}}</div>
            <hr />
-           <div class="text-sm my-3 flex flex-row"> D2 : {{lineup1.D2.pair.pairInfo}}</div>
+           <div class="text-sm my-3 flex flex-row">
+                <div>
+                    <img v-if="lineup1.D2.pair.totalUTR > lineup2.D2.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+                </div>
+           D2 : {{lineup1.D2.pair.pairInfo}}</div>
            <hr />
-           <div class="text-sm my-3 flex flex-row"> D3 : {{lineup1.D3.pair.pairInfo}}</div>
+           <div class="text-sm my-3 flex flex-row">
+            <div>
+                <img v-if="lineup1.D3.pair.totalUTR > lineup2.D3.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+            </div>
+           D3 : {{lineup1.D3.pair.pairInfo}}</div>
            <hr />
-           <div class="text-sm my-3 flex flex-row"> MD : {{lineup1.MD.pair.pairInfo}}</div>
+           <div class="text-sm my-3 flex flex-row">
+            <div>
+                <img v-if="lineup1.MD.pair.totalUTR > lineup2.MD.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+            </div>
+           MD : {{lineup1.MD.pair.pairInfo}}</div>
            <hr />
-           <div class="text-sm my-3 flex flex-row"> WD : {{lineup1.WD.pair.pairInfo}}</div>
+           <div class="text-sm my-3 flex flex-row">
+            <div>
+                <img v-if="lineup1.WD.pair.totalUTR > lineup2.WD.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+            </div>
+           WD : {{lineup1.WD.pair.pairInfo}}</div>
            <hr />
          </div>
          <div class="border-transparent rounded-lg text-center p-5 mx-auto md:mx-0 my-2 bg-gray-700 text-white font-medium z-10 shadow-lg">
            <div class="font-bold text-2xl"> Team {{ teamName2 }} </div>
            <hr />
-           <div class="text-sm my-3 flex flex-row"> D1 : {{lineup2.D1.pair.pairInfo}}</div>
-           <hr />
-           <div class="text-sm my-3 flex flex-row"> D2 : {{lineup2.D2.pair.pairInfo}}</div>
-           <hr />
-           <div class="text-sm my-3 flex flex-row"> D3 : {{lineup2.D3.pair.pairInfo}}</div>
-           <hr />
-           <div class="text-sm my-3 flex flex-row"> MD : {{lineup2.MD.pair.pairInfo}}</div>
-           <hr />
-           <div class="text-sm my-3 flex flex-row"> WD : {{lineup2.WD.pair.pairInfo}}</div>
-           <hr />
+              <div class="text-sm my-3 flex flex-row">
+                   <div>
+                       <img v-if="lineup1.D1.pair.totalUTR < lineup2.D1.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+                   </div>
+              D1 : {{lineup2.D1.pair.pairInfo}}</div>
+              <hr />
+              <div class="text-sm my-3 flex flex-row">
+                   <div>
+                       <img v-if="lineup1.D2.pair.totalUTR < lineup2.D2.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+                   </div>
+              D2 : {{lineup2.D2.pair.pairInfo}}</div>
+              <hr />
+              <div class="text-sm my-3 flex flex-row">
+               <div>
+                   <img v-if="lineup1.D3.pair.totalUTR < lineup2.D3.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+               </div>
+              D3 : {{lineup2.D3.pair.pairInfo}}</div>
+              <hr />
+              <div class="text-sm my-3 flex flex-row">
+               <div>
+                   <img v-if="lineup1.MD.pair.totalUTR < lineup2.MD.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+               </div>
+              MD : {{lineup2.MD.pair.pairInfo}}</div>
+              <hr />
+              <div class="text-sm my-3 flex flex-row">
+               <div>
+                   <img v-if="lineup1.WD.pair.totalUTR < lineup2.WD.pair.totalUTR " src="/win_1262465.png" width="30" height="30" alt="Win"/>
+               </div>
+              WD : {{lineup2.WD.pair.pairInfo}}</div>
+              <hr />
          </div>
      </div>
 </template>
